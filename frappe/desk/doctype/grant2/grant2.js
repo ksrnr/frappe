b83d75fe7d49bd02c0b0085b3565543795ac50f9
Adam Dawoodjee <adam.daveed@gmail.com>
2021-12-19 14:31:41 +0000

    migrate

---
+++ frappe/desk/doctype/grant2/grant2.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Frappe Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Grant2', {
+	// refresh: function(frm) {
+
+	// }
+});
